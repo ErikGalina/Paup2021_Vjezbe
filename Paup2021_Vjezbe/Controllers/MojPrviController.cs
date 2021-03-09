346e688018904df0d29899951244a08a27dc1d2f
mev <mev@PC-19-27>
2021-03-09 17:47:46 +0100

    Prvi commit

--- Paup2021_Vjezbe/Controllers/MojPrviController.cs
+++ Paup2021_Vjezbe/Controllers/MojPrviController.cs
@@ -31,8 +31,8 @@ namespace Paup2021_Vjezbe.Controllers
 
         public ActionResult Student()
         {
-            ViewBag.Ime = "Ivo";
-            ViewBag.Prezime = "Perić";
+            ViewBag.Ime = "Kite";
+            ViewBag.Prezime = "Milić";
             ViewBag.GodinaRodjenja = 1995;
 
             return View();
